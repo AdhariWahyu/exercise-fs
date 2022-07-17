bb1820ae9844d0f08a87e2569c8ab3a779f57c28
AdhariWahyu <wahyuwidito24@gmail.com>
2022-07-17 15:15:22 +0700

    day 2

---
+++ javascript.js
@@ -0,0 +1,99 @@
+// LOOPING
+
+// for(var i=1; i<10; i++){
+//     console.log(i)
+// }
+
+// var num = 13;
+// for(var i = 1; i <= num; i++){
+//     if(i % 2 === 0){
+//         console.log(i + " " + "bilangan genap")
+//     } else{
+//         console.log(i + " " + "bilangan ganjil")
+//     }
+// }
+
+
+// var str = "sarapan";
+
+// for(var i = 0; i < str.length; i++){
+//     if (i % 2 === 0){
+//         console.log(str[i].toLocaleUpperCase())
+//     } else{
+//         console.log(str[i])
+//     }
+// }
+
+// var str = "sarapan"
+// var temp = ""
+
+// for(var i = 0; i < str.length; i++){
+//     if(i % 2 === 0){
+//         temp = temp + str[i].toUpperCase()
+//     } else{
+//         temp = temp + str[i]
+//     }
+// }
+// console.log(temp)
+
+// var num = 3;
+// var temp = ""
+// for (var i = 0; i < num; i++){
+//     for(var j = 0; j < num; j++){
+//         temp += "#"
+//     }
+//     console.log(temp)
+// }
+
+// var num = 3;
+// var temp = ""
+// for (var i = 0; i < num; i++){
+//     for(var j = 0; j < num; j++){
+//         temp += "#"
+//         console.log(temp)
+//     }
+// }
+
+// var num = 3;
+// var temp = ""
+// for (var i = 0; i < num; i++){
+//     for(var j = 0; j < num; j++){
+//         temp += "#"
+//     }
+//     console.log(temp)
+//     temp = ""
+// }
+
+// var num = 3;
+// var temp = ""
+
+// for (var i = 0; i < num; i++){
+//      for(var j = 0; j < num; j++){
+//          temp += "#"
+//          console.log(temp)
+//      }
+//      temp = ""
+//  }
+
+// var num = 5;
+// var temp = ""
+
+// for (var i = 0; i < num; i++) {
+//     for (var j = 0; j <= i; j++) {
+//         temp += "#"
+//     }
+//     console.log(temp)
+//     temp = ""
+// }
+
+var num = 5;
+var temp = ""
+var char = "$"
+
+for (var i = 0; i < num; i++) {
+    for (var j = 0; j <= i; j++) {
+        temp += char
+    }
+    console.log(temp)
+    temp = ""
+}
